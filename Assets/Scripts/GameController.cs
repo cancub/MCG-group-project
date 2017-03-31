fafa516f9fa882cd5f60a42cd50e366109c02609
cancub <cancub@live.com>
2017-03-31 01:18:50 -0400

    grid created and visual

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -16,19 +16,23 @@ using UnityEngine;
 public class GameController : MonoBehaviour {
 
 	private List<List<bool>> map;
+	private List<List<GameObject>> cubes;
 	private Vector2 playerLocation;
 	public GameObject ground;
-	public GameObject borderPrefab;
 	private Vector2 size;
+	private Vector2 topLeft;
 
 
 	// Use this for initialization
 	void Start () {
 		map = new List<List<bool>> ();
+		cubes = new List<List<GameObject>> ();
+		size = 10f * new Vector2 (ground.transform.localScale.x, ground.transform.localScale.z);
+		topLeft = new Vector2 (size.x / -2f, size.y / 2f);
+		print (size);
+		print (topLeft);
 		BuildInitialMap ();
 		DrawMap ();
-		size = 10f * new Vector2 (ground.transform.localScale.x, ground.transform.localScale.z);
-		print (size);
 	}
 	
 	// Update is called once per frame
@@ -43,14 +47,20 @@ public class GameController : MonoBehaviour {
 		// the map can hold 12 x 12 cells to build the network, so the initial map
 		// will simply have roads set as the locations at odd columns and rows (zero indexed)
 
-		for (int i = 0; i < size.x; i++) {
-			map[i] = new List<bool>;
-			for (int j = 0; j < size.y; j++) {
-				if (i%2==1 && j%2==1){
-					map[i][j] = true;
+		for (int i = 0; i < size.x + 1; i++) {
+			map.Add(new List<bool>());
+			cubes.Add(new List<GameObject>());
+			for (int j = 0; j < size.y + 1; j++) {
+				// the cubes will be used as a visual reference for the pathfinding
+				cubes[i].Add(GameObject.CreatePrimitive(PrimitiveType.Cube));
+				if (i%2==1 || j%2==1){
+					map[i].Add(true);
+					cubes[i][j].transform.position = new Vector3(topLeft.x+(float)i,topLeft.y - (float)j,1f);
 				} else {
-					map[i][j] = false;
+					map[i].Add(false);
+					cubes[i][j].transform.position = new Vector3(topLeft.x+(float)i,topLeft.y - (float)j,0);
 				}
+
 			}
 		}
 	}
